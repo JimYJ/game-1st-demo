b8501f00b1a6abcd4c9801f74aa011960a68c855
JimYJ <yblues@hotmail.com>
2020-03-08 02:44:38 +0800

    WIP

--- temp/quick-scripts/assets/scripts/player.js
+++ temp/quick-scripts/assets/scripts/player.js
@@ -37,21 +37,65 @@ cc.Class({
         return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
     },
 
+    onKeyDown: function onKeyDown(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = true;
+                break;
+        }
+    },
+    onKeyUp: function onKeyUp(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = false;
+                break;
+        }
+    },
+
+
     onLoad: function onLoad() {
         // 初始化跳跃动作
         this.jumpAction = this.setJumpAction();
         this.node.runAction(this.jumpAction);
+        this.accLeft = false;
+        this.accRight = false;
+        this.xSpeed = 0;
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
     },
 
-    // LIFE-CYCLE CALLBACKS:
+    onDestroy: function onDestroy() {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
 
-    // onLoad () {},
+    start: function start() {},
 
-    start: function start() {}
-}
 
-// update (dt) {},
-);
+    update: function update(dt) {
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // 限制主角的速度不能超过最大值
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // 根据当前速度更新主角的位置
+        this.node.x += this.xSpeed * dt;
+    }
+});
 
 cc._RF.pop();
         }
